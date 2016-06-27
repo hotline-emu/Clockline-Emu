4b14807911860addc78bf6cbe66044af74f48d55
James Galecki <james.galecki@dominionenterprises.com>
2016-06-27 15:40:30 -0400

    Location now works
    It is not entirely accurate though, as I am in Norfolk, but it says Virginia Beach.

--- ClockLine_Emu/Form1.cs
+++ ClockLine_Emu/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Device.Location;
+using System.Net;
 
 namespace ClockLine_Emu
 {
@@ -18,7 +18,7 @@ namespace ClockLine_Emu
         public Form1()
         {
             InitializeComponent();
-            initializeLocation();
+            initializeLocation(); // Set the location on the clock.
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,24 +26,33 @@ namespace ClockLine_Emu
 
         }
 
+        /// <summary>
+        /// Update the timer on each tick.
+        /// Currently set to only report Hours:Minutes, but easily configurable to whatever.
+        /// View MSDN documentation if you want to modify this yourself.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void timer1_Tick(object sender, EventArgs e)
         {
             label1.Text = DateTime.Now.ToString("hh:mm tt");
         }
 
+        /// <summary>
+        /// When starting the program, get the city based off of your IP.
+        /// </summary>
         private void initializeLocation()
         {
-            GeoCoordinateWatcher myLocation = new GeoCoordinateWatcher(GeoPositionAccuracy.High);            
-            myLocation.TryStart(false, TimeSpan.FromMilliseconds(1000));
-
-            physicalLocation = "Norfolk, VA"; // TODO, actually get the location.
-
-            var latitude = myLocation.Position.Location.Latitude;
-            var longitude = myLocation.Position.Location.Longitude;
-            
-            location.Text = String.Format("{0}, {1}", latitude, longitude);
-
-            
+            try
+            {
+                string netLocation = new WebClient().DownloadString("http://ip-api.com/json"); // First available free API found. Not the most accurate.
+                string[] netLocationArray = netLocation.Split(','); // IDK why I am not allowed to do this on one line.
+                location.Text = netLocationArray[1].Replace("\"city\":\"", "").TrimEnd('\"'); // Ugly AF, give me just the city.
+            }
+            catch (Exception)
+            {
+                location.Text = ""; // More than likely, you aren't connected to the internet.
+            }
         }
     }
 }
